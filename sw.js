2bd31f864a40cce0be433624c6d35a092b4d494e
CassianaSL <cassianasilveira64@gmail.com>
2019-10-16 10:43:46 -0300

    commit

--- sw.js
+++ sw.js
@@ -29,29 +29,10 @@ self.addEventListener('activate', function activator(event) {
   }));
 });
 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request).then(function(response) {
-//         if (response) {
-//           return response;
-//         }
-//         var fetchRequest = event.request.clone();
-
-//         return fetch(fetchRequest).then(function(response) {
-//             if(!response || response.status !== 200 || response.type !== 'basic') {
-//               return response;
-//             }
-
-//             var responseToCache = response.clone();
-
-//             caches.open(CACHE_NAME)
-//               .then(function(cache) {
-//                 cache.put(event.request, responseToCache);
-//               });
-
-//             return response;
-//           }
-//         );
-//       })
-//     );
-// });
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
