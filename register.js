fd3e492c3d6a725f1689d46bae017986436453ea
CassianaSL <cassianasilveira64@gmail.com>
2019-10-16 10:31:38 -0300

    commit

--- register.js
+++ register.js
@@ -1,5 +1,7 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-  .then(function () {console.log('service worker registered');})
-  .catch(function () {console.warn('service worker failed'); });
-}
+console.log('serviceWorker' in navigator);
+
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('/sw.js')
+//   .then(function () {console.log('service worker registered');})
+//   .catch(function () {console.warn('service worker failed'); });
+// }
